38c7a319f2dc478040fada5a0d6c9d21e66a06cd
ktf <not.discostoo@gmail.com>
2012-12-06 15:58:08 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 
 setup(name='pcapitate',
-      version='0.1.0',
+      version='0.2.0',
       description='Parses raw pcap files for a friendlier snooping experience',
       author='Keith Fancher',
       author_email='keith.fancher@gmail.com',
